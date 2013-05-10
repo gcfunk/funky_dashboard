caffb41cba48f44c26d76e84d1f509ff3324cb50
Greg Funk <gfunk@thinkthroughmath.com>
2013-05-09 20:44:52 -0400

    Parameterize forecast params + new forecast job body #loveopensource

--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -2,19 +2,19 @@ require 'net/https'
 require 'json'
 
 # Forecast API Key from https://developer.forecast.io
-forecast_api_key = ""
+forecast_api_key = ENV['FORECAST_IO_API_KEY']
 
 # Latitude, Longitude for location
-forecast_location_lat = "45.429522"
-forecast_location_long = "-75.689613"
+forecast_location_lat = ENV['FORECAST_LAT']
+forecast_location_long = ENV['FORECAST_LONG']
 
 # Unit Format
 # "us" - U.S. Imperial
 # "si" - International System of Units
 # "uk" - SI w. windSpeed in mph
-forecast_units = "si"
+forecast_units = "us"
 
-SCHEDULER.every '5m', :first_in => 0 do |job|
+SCHEDULER.every '15m', :first_in => 0 do |job|
   http = Net::HTTP.new("api.forecast.io", 443)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_PEER
@@ -23,9 +23,15 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
   forecast_current_temp = forecast["currently"]["temperature"].round
   forecast_current_icon = forecast["currently"]["icon"]
   forecast_current_desc = forecast["currently"]["summary"]
-  forecast_next_desc    = forecast["minutely"]["summary"]
-  forecast_next_icon    = forecast["minutely"]["icon"]
-  forecast_later_desc   = forecast["hourly"]["summary"]
-  forecast_later_icon   = forecast["hourly"]["icon"]
-  send_event('seattle-forecast', { current_temp: "#{forecast_current_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", next_icon: "#{forecast_next_icon}", next_desc: "#{forecast_next_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
+  if forecast["minutely"] # sometimes this is missing from the response. I don't know why
+    forecast_next_desc = forecast["minutely"]["summary"]
+    forecast_next_icon = forecast["minutely"]["icon"]
+  else
+    puts "Did not get minutely forecast data again"
+    forecast_next_desc = "No data"
+    forecast_next_icon = ""
+  end
+  forecast_later_desc = forecast["hourly"]["summary"]
+  forecast_later_icon = forecast["hourly"]["icon"]
+  send_event('forecast', { current_temp: "#{forecast_current_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", next_icon: "#{forecast_next_icon}", next_desc: "#{forecast_next_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
 end
