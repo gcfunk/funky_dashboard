b50290dd9f287ec625ac2756e1e1511a171a39d9
Greg Funk <gfunk@thinkthroughmath.com>
2013-05-23 11:36:39 -0400

    Adds hourly weather forecast

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ This project is Greg automating personal data gathering and display.  I'm going 
 [http://shopify.github.io/dashing/](http://shopify.github.io/dashing/)
 
 ###Forecast Widget
-`export FORECAST_IO_API_KEY=your_api_key`
 
-`export FORECAST_LAT=your_latitude`
+`echo 'FORECAST_IO_API_KEY=your_api_key' >> .env`
 
-`export FORECAST_LONG=your_longitude`
+`echo 'FORECAST_LAT=your_latitude' >> .env`
+
+`echo 'FORECAST_LONG=your_longitude' >> .env`
 
 * Forecast API Key from [developer.forecast.io](developer.forecast.io)
 * Latitude and Longitude from [geocoder.ca](geocoder.ca) 
--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -1,5 +1,7 @@
 require 'net/https'
 require 'json'
+require 'rubygems'
+require 'active_support/all'
 
 # Forecast API Key from https://developer.forecast.io
 forecast_api_key = ENV['FORECAST_IO_API_KEY']
@@ -14,7 +16,9 @@ forecast_location_long = ENV['FORECAST_LONG']
 # "uk" - SI w. windSpeed in mph
 forecast_units = "us"
 
-SCHEDULER.every '15m', :first_in => 0 do |job|
+timezone = ActiveSupport::TimeZone.new("Eastern Time (US & Canada)")
+
+SCHEDULER.every '10s', :first_in => 0 do |job|
   http = Net::HTTP.new("api.forecast.io", 443)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_PEER
@@ -33,5 +37,20 @@ SCHEDULER.every '15m', :first_in => 0 do |job|
   end
   forecast_later_desc = forecast["hourly"]["summary"]
   forecast_later_icon = forecast["hourly"]["icon"]
-  send_event('forecast', { current_temp: "#{forecast_current_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", next_icon: "#{forecast_next_icon}", next_desc: "#{forecast_next_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
+  event = {
+      current_temp: "#{forecast_current_temp}&deg;",
+      current_icon: "#{forecast_current_icon}",
+      current_desc: "#{forecast_current_desc}",
+      next_icon: "#{forecast_next_icon}",
+      next_desc: "#{forecast_next_desc}",
+      later_icon: "#{forecast_later_icon}",
+      later_desc: "#{forecast_later_desc}"
+  }
+  forecast_hourly_data = forecast["hourly"]["data"].each_with_index do |data, index|
+    event.merge!({
+      "hourly_#{index}_icon".to_sym => data["icon"],
+      "hourly_data_#{index}".to_sym => "#{Time.at(data['time']).to_datetime.in_time_zone(timezone).to_time.hour} #{data['temperature'].round}&deg;, #{((data['precipProbability'] || 0) * 100).round}%"
+    })
+  end
+  send_event('forecast', event)
 end
